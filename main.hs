fb26167d06bf86a04b4528bd3b47acf8ac5695e4
George <geo.rog09@gmail.com>
2020-10-20 14:56:25 +0000

    Question 1.2

--- main.hs
+++ main.hs
@@ -9,6 +9,15 @@ rule1 []
 rule1 (x:xs)
   = x `notElem` xs && rule1 xs
 
+-- Question 1.2
+rule2 :: [Int] -> Bool
+rule2 []
+  = True
+rule2 [x]
+  = True
+rule2 (x:y:xs)
+  = x `mod` 2 /= y `mod` 2 && rule2 (y:xs)
+
 main :: IO()
 main
-  = putStrLn (show (rule1 (tupleToList (1,2,3,3,5,6))))
+  = putStrLn (show (rule2 (tupleToList (4,9,6,3,0,7))))
