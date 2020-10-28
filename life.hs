d5f0d64b0aa9c90b2c009064f567449da63ea189
George <geo.rog09@gmail.com>
2020-10-28 16:55:07 +0000

    createGrid

--- life.hs
+++ life.hs
@@ -3,6 +3,24 @@ pretty :: [[String]] -> String
 pretty xs
   = unlines $ concat $ xs
 
+-- Question 2.2
+type Point
+  = ( Int, Int )
+
+glider :: [ Point ]
+glider
+  = [(0,2),(1,3),(2,1),(2,2),(2,3)]
+
+--visualisation :: Int -> Int -> [[Point]] -> [[String]]
+
+createGrid :: Int -> Int -> [[String]]
+createGrid w h
+  = replicate h [concat $ replicate w "."]
+
 main :: IO()
 main
-  = putStrLn( show( pretty [[['a', 'b'], ['c', 'd']], [['e', 'f'], ['g', 'h']], [['i', 'j'], ['k', 'l']]]))
+  = putStrLn( show (createGrid 5 5))
+
+--main :: IO()
+--main
+--  = putStrLn( show( pretty [[['a', 'b'], ['c', 'd']], [['e', 'f'], ['g', 'h']], [['i', 'j'], ['k', 'l']]]))
