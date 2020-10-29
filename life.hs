a3e194593ba2b6fb16cadda454e756a204827bf4
George <geo.rog09@gmail.com>
2020-10-29 15:31:29 +0000

    Question 2.2

--- life.hs
+++ life.hs
@@ -5,22 +5,40 @@ pretty xs
 
 -- Question 2.2
 type Point
-  = ( Int, Int )
+  = (Int, Int)
 
-glider :: [ Point ]
+glider :: [Point]
 glider
   = [(0,2),(1,3),(2,1),(2,2),(2,3)]
 
---visualisation :: Int -> Int -> [[Point]] -> [[String]]
+gridPoints :: Int -> [Point] -> [[Int]]
+gridPoints height xs
+  = [rowPoints i xs | i <- [0..(height - 1)]]
 
-createGrid :: Int -> Int -> [[String]]
-createGrid w h
-  = replicate h [concat $ replicate w "."]
+rowPoints :: Int -> [Point] -> [Int]
+rowPoints i []
+  = []
+rowPoints i ((a,b):xs)
+  | i == b = a : (rowPoints i xs)
+  | otherwise = rowPoints i xs
+
+createGrid :: Int -> Int -> [Point] -> [String]
+createGrid w h xs
+  = map (createRow w) (gridPoints h xs)
+
+createRow :: Int -> [Int] -> String
+createRow width xs
+  = [createPoint i xs | i <- [0..(width - 1)]]
+
+createPoint :: Int -> [Int] -> Char
+createPoint i xs
+  | i `elem` xs = '#'
+  | otherwise = '.'
+
+visualisation :: Int -> Int -> [[Point]] -> [[String]]
+visualisation w h xs
+  = map (createGrid w h) xs
 
 main :: IO()
 main
-  = putStrLn( show (createGrid 5 5))
-
---main :: IO()
---main
---  = putStrLn( show( pretty [[['a', 'b'], ['c', 'd']], [['e', 'f'], ['g', 'h']], [['i', 'j'], ['k', 'l']]]))
+  = putStrLn( show (visualisation 5 5 [ glider ] ))
