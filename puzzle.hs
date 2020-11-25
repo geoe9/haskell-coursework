b44cc4a18b0cf2ab3ab12c08c33288d1c4f7f39d
George <48719453+geoe9@users.noreply.github.com>
2020-11-25 14:46:39 +0000

    Update puzzle.hs

--- puzzle.hs
+++ puzzle.hs
@@ -70,7 +70,7 @@ possibles
 {-- Question 1.6 --}
 isSolution :: (Int, Int, Int, Int, Int, Int) -> Bool
 isSolution x
-  = rule1 xs && rule2 xs && rule3 xs && rule4 xs
+  = rule1 x && rule2 x && rule3 x && rule4 x
 
 main :: IO()
 main
